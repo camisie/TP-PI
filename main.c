835d94474bb22e21a5edd4756ba36b073abdc736
magflores <magflores@itba.edu.ar>
2021-07-05 19:55:10 -0300

    ejecutable y main

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
+#include "moviesADT.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+int main(int count, char *doc[]){
+    //chequeo que la cantidad de datos ingresada sea la correcta
+    if (count != 8)
+        {
+            fprintf(stderr, "Cantidad incorrecta de datos\n");
+            exit(1);
+        }
+    //
+    
+}
