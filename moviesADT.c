25dd16870f0b9ca6945e05286a5a39d159f3ea1b
magflores <magflores@itba.edu.ar>
2021-07-05 17:17:41 -0300

    mostVotedNode
    Nodo para la pelicula/serie con mas votos

--- moviesADT.c
+++ moviesADT.c
@@ -3,3 +3,13 @@
 #include<string.h>
 #include<stdlib.h>
 
+typedef struct mostVotedNode{
+  char titleType;
+  char *primaryTitle;
+  unsigned int startYear;
+  unsigned int endYear;
+  char *genres;
+  unsigned int averageRating;
+  unsigned int numVotes;
+  unsigned int runtimeMinutes;
+}mostVotedNode;
