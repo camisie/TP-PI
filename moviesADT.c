977d6ce20957cf50c6867d0d57b368a296d9eea1
magflores <magflores@itba.edu.ar>
2021-07-05 17:56:44 -0300

    updates .c

--- moviesADT.c
+++ moviesADT.c
@@ -3,13 +3,23 @@
 #include<string.h>
 #include<stdlib.h>
 
-typedef struct mostVotedNode{
-  char titleType;
-  char *primaryTitle;
-  unsigned int startYear;
-  unsigned int endYear;
-  char *genres;
-  unsigned int averageRating;
-  unsigned int numVotes;
-  unsigned int runtimeMinutes;
-}mostVotedNode;
+//Este es el nodo para la lista de generos
+typedef struct genreNode{
+    char *name;
+    size_t sizeF; //cantidad de peliculas de ese genero
+    struct genreNode *tail;
+}TGenre;
+
+//estructura del nodo para la lista de años
+typedef struct yearNode{
+    TGenre *first;
+    size_t totalF; //cantidad total de peliculas en ese año
+    size_t totalS; //cantidad total de series en ese año
+    TData *bestMovie;
+    TData *bestSerie;
+    struct yearNode *tail;
+}TYear;
+
+typedef struct moviesCDT{
+    TYear *first;
+}moviesCDT;
